6aefc7b0bea08c83d5a60923626792a7c39a023c
DuongDangQuan <quandd.23it@vku.udn.vn>
2024-11-12 21:20:52 +0700

    adjust login

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const app = express();
 
 app.set('view engine','ejs');
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
-
-app.get("/",(req,res) =>{
-    res.render("login");
-
+app.post("/",(req,res) =>{
+    console.log(req.body);
 });
+
+app.get("/login",(req,res) =>{
+    res.render("login");
+});
+
+// app.post("/login",(req,res) =>{
+//     console.log("abc");
+//     console.log(req.body);
+// })
 
 app.listen(3000);
